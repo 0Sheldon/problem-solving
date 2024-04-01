f90ccea4b0da81e3d340bb3e11d5737dc3dbb95a
0Sheldon <ryudb030+0@gmail.com>
2024-04-01 23:02:38 +0900

    문자열안에 문자열

---
+++ study/src/lv0/_120908.java
@@ -0,0 +1,9 @@
+package lv0;
+
+public class _120908 {
+    public int solution(String str1, String str2) {
+        int index = str1.indexOf(str2);
+        if(index == -1) return 2;
+        return 1;
+    }
+}
