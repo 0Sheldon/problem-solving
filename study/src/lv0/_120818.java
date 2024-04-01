ad306f753d5e3018d17ca500b9793b854b2cabe3
0Sheldon <ryudb030+0@gmail.com>
2024-04-01 10:38:44 +0900

    옷가게 할인 받기

---
+++ study/src/lv0/_120818.java
@@ -0,0 +1,14 @@
+package lv0;
+
+public class _120818 {
+    public int solution(int price) {
+        if(price >= 500_000) {
+            return (int)(price - price * 0.2);
+        } else if (price >= 300_000) {
+            return (int)(price - price * 0.1);
+        } else if (price >= 100_000) {
+            return (int)(price - price * 0.05);
+        }
+        return price;
+    }
+}
