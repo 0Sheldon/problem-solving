e4ba49d215cf97859e0b9d8d358f8f57dacafc38
0Sheldon <ryudb030+0@gmail.com>
2024-04-02 15:34:08 +0900

    문자열의 뒤의 n글자

---
+++ study/src/lv0/_181910.java
@@ -0,0 +1,8 @@
+package lv0;
+
+public class _181910 {
+    public String solution(String my_string, int n) {
+        int index = my_string.length() - n;
+        return my_string.substring(index);
+    }
+}
